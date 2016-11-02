69652b3bb7a6672de0784b899039c0ce86c8ab7f
ilerik <erik.lite@gmail.com>
2016-11-02 18:19:08 +0300

    Implementation of command line tool.

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ SolutionResult solve(double mu, double alpha, F fitness, double tolerance) {
       status.p = x.block<N + 1, 1>(0, 0).sum();
       status.q = x.block<N + 1, 1>(N + 1, 0).sum();
 
-      std::cout << "R = " << R << ", s = " << s << ",p = " << status.p << ", q = " << status.q << " " << std::endl;
+      std::cout << "R = " << R << ", s = " << status.s << ",p = " << status.p << ", q = " << status.q << " " << std::endl;
 
       // Stop criteria
       if (residual_norm < tolerance) {
@@ -103,7 +103,7 @@ SolutionResult solve(double mu, double alpha, F fitness, double tolerance) {
       };
 
       // Non-selective phase detection
-      if (s < tolerance) {
+      if (status.s < tolerance) {
         std::cout << "non-selective phase reached, calculation stoped" << std::endl;
         status.type = SolutionType::NonSelective;
         break;
@@ -126,7 +126,7 @@ SolutionResult solve(double mu, double alpha, F fitness, double tolerance) {
 };
 
 
-int main()
+int main(int argc, char *argv[])
 {
   {
     const int L = 400;
@@ -134,91 +134,36 @@ int main()
     double alpha = 1; //0.1      
     //auto fitness = [] (double x) { return (x + 1); };
     //auto fitness = [] (double x) { return x > 0.8 ? 5*x*x : 0.0; };
-    auto fitness = [] (double x) { return 5*x*x; };
+    //auto fitness = [] (double x) { return 5*x*x; };    
     //auto fitness = [] (double x) { return x == 1.0 ? 10.0 : 0.0; };
 
-    /*std::ofstream ofs("theory.dat");
-    ofs << R"(VARIABLES = "mu", "R_mt", "R_mx")";
-    double R_mt = -1e50;
-    double R_mx = -1e50;
-    for ( mu = 0.5; mu < 10; mu += 0.1) {
-      R_mt = -1e50;
-      R_mx = -1e50;
-      for (double x = 1.0; x >= -1.0; x -= 1e-2) {
-        double R1 = fitness(x) + mu * (std::sqrt(1 - x * x) - 1);
-        if (R1 > R_mt) R_mt = R1;
-        double R2 = fitness(x) - alpha + std::sqrt(1 - x * x) - 1;
-        if (R2 > R_mx) R_mx = R2;
-      };
-      ofs << mu << " " << R_mt << " " << R_mx << std::endl;
+    if (argc != 7) {
+      std::cout << "usage: <alpha> <mu> <a0> <a1> <a2> <a3>" << std::endl;
+      std::cout << "where fitness function f(x) = a0 + a1*x + a2*x^2 + a3*x^3" << std::endl;
+      return 0;
     };
-
-    return 0;*/
+    
+    alpha = std::stod(argv[1]);
+    mu = std::stod(argv[2]);
+    double a0 = std::stod(argv[3]);
+    double a1 = std::stod(argv[4]);
+    double a2 = std::stod(argv[5]);
+    double a3 = std::stod(argv[6]);
+    auto fitness = [=] (double x) { return a0 + a1*x + a2*x*x + a3*x*x*x; };
 
     std::cout << "parameters : " << std::endl;
+    std::cout << "L = " << L << std::endl;
     std::cout << "alpha = " << alpha << std::endl;
     std::cout << "mu    = " << mu << std::endl;
-    //std::cout << "R_mt  = " << R_mt << std::endl;
-    //std::cout << "R_mx  = " << R_mx << std::endl;
 
     auto status = solve<L>(mu, alpha, fitness, 1e-10);
 
     std::cout << "parameters : " << std::endl;
     std::cout << "L = " << L << std::endl;
     std::cout << "alpha = " << alpha << std::endl;
-    std::cout << "mu    = " << mu << std::endl;
-    //std::cout << "R_mt  = " << R_mt << std::endl;
-    //std::cout << "R_mx  = " << R_mx << std::endl;
+    std::cout << "mu    = " << mu << std::endl;    
     std::cout << "result : " << std::endl;
     std::cout << "R = " << status.R << ", s = " << status.s << ", p = " << status.p << ", q = " << status.q << std::endl;   
   }
 
-  { // Phases in parametric space
-    //const int N = 100;
-    //std::ofstream ofs("phases.dat");
-    //auto fitness = [=] (int i) { 
-    //  double x = 1.0 * i / N;
-    //  double f = (1 - x)*(1 - x);
-    //  return f; 
-    //};
-    //int n_mu = 15;
-    //double mu_min = 0.01;
-    //double mu_max = 2.0;
-    //int n_alpha = 20;
-    //double alpha_min = 0.01;
-    //double alpha_max = 1.5;
-    ////std::vector<SolutionType>types    
-    //for (auto i_alpha = n_alpha; i_alpha >= 0; i_alpha--) {
-    //  auto alpha = alpha_min + i_alpha * (alpha_max - alpha_min) / n_alpha;
-    //  //std::cout << alpha << " : ";
-    //    for (auto i_mu = 0; i_mu <= n_mu; i_mu++) {
-    //    auto mu = mu_min + i_mu * (mu_max - mu_min) / n_mu;        
-    //    
-    //    // solve
-    //    auto status = solve<100>(mu, alpha, fitness, 1e-10);
-    //    //std::cout << "mu = " << mu << ", alpha = " << alpha;
-    //    //std::cout << ", phase = ";
-    //    switch (status.type) { 
-    //    case SolutionType::NonSelective: { std::cout << "NS "; break; };
-    //    case SolutionType::Mutator: { std::cout << "MT "; break; };
-    //    case SolutionType::Mixed: { std::cout << "MX "; break; };
-    //    }
-    //    //std::cout << std::endl;
-    //  }      
-    //  std::cout << std::endl;
-    //  
-    //}
-
-  }
-    
-  /*
-  using Equations = MutatorEquation<n>;
-  using EquationsNumericalDiff = Eigen::NumericalDiff<Equations>;
-  EquationsNumericalDiff Af;
-  Af(x, fvec);
-  std::cout << fvec;
-  Eigen::LevenbergMarquardt<EquationsNumericalDiff> lm(Af);
-  Eigen::LevenbergMarquardtSpace::Status status = lm.minimize(x);
-  std::cout << "status: " << status << std::endl;  
-  std::cout << "x that minimizes the function: " << std::endl << x << std::endl;  */
 }
